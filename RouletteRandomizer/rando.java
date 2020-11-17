73c6ecfc94a2b57243c6d1c47e4327eb3fa09f5c
Zach <zach.t.gonzales@gmail.com>
2020-11-16 21:05:20 -0600

    test

--- RouletteRandomizer/rando.java
+++ RouletteRandomizer/rando.java
@@ -3,6 +3,14 @@ public class rando{
   public static void main(String[] args){
 
 
+
+    System.out.println("Greetings adventurer. Please let us know your Job, and Level");
+    job jTester = new job("Samurai","DPS",80);
+
+    System.out.println("I see that you are a " + jTester.getName() + " at level " + jTester.getLevel() + "!");
+
+    System.out.println("What roulettes have you run today so far?");
+
           String welcome = "This is not an ukemi!";
           System.out.println(welcome);
           Random rand = new Random();
@@ -33,5 +41,9 @@ public class rando{
             default:
               System.out.println("I AM THE STORM THAT IS APPROACHING!");
           }
+
+          System.out.println("Based on your answer we think you should run: " + "[Placeholder for duty to be run]" + "!");
+
+          System.out.println("Would you like to run this one or try again? :) ");
         }
 }
--- RouletteRandomizer/rouletteBoiz.java
+++ RouletteRandomizer/rouletteBoiz.java
@@ -9,7 +9,20 @@ import java.util.Random;
    A method to store info about a user
    A random number generator
    */
+   //create a loop or a sequential that asks
+   //"Any other roulettes already done?"
+   //if they say yes, accept input if not, close loop
+   //while(more roulettes = true then loop: more roulettes = false close loop)
 
+   //accept input here for duties that have been run
+   //probably use an arraylist to hold duties that can be run or have been run.
+   //use a for each loop to walk through set duties.
+   //place holder for user input, should be assumed that they will only be using one class
+   //job jTester = new job();
+   //ask for name - input1
+   //create a conditional statement
+   //if input1 = dps job, then type = DPs etc. etc.
+   //ask for level -input2
    //Main method
 public class rouletteBoiz
 {
@@ -18,59 +31,45 @@ public class rouletteBoiz
 
 
 
-      String goodbye = "This is not an Ukemi";
-      System.out.println("Greetings adventurer. Please let us know your Job, and Level");
-      job jTester = new job("Samurai","DPS",80);
-      //place holder for user input, should be assumed that they will only be using one class
-      //job jTester = new job();
-      //ask for name - input1
-      //create a conditional statement
-      //if input1 = dps job, then type = DPs etc. etc.
-      //ask for level -input2
-      System.out.println("I see that you are a " + jTester.getName() + " at level " + jTester.getLevel() + "!");
+        System.out.println("Greetings adventurer. Please let us know your Job, and Level");
+        job jTester = new job("Samurai","DPS",80);
 
-      System.out.println("What roulettes have you run today so far?");
-      //create a loop or a sequential that asks
-      //"Any other roulettes already done?"
-      //if they say yes, accept input if not, close loop
-      //while(more roulettes = true then loop: more roulettes = false close loop)
+        System.out.println("I see that you are a " + jTester.getName() + " at level " + jTester.getLevel() + "!");
 
-      //accept input here for duties that have been run
-      //probably use an arraylist to hold duties that can be run or have been run.
-      //use a for each loop to walk through set duties.
-      String welcome = "This is not an ukemi!";
-      System.out.println(welcome);
-      Random rand = new Random();
-      int rouletteCap = 6;
-      //
-      int rouletteResult = rand.nextInt(rouletteCap);
-      System.out.println(rouletteResult);
+        System.out.println("What roulettes have you run today so far?");
 
-      switch(rouletteResult){
-        case 1:
-          System.out.println("Leveling roulette!");
-          break;
-        case 2:
-          System.out.println("Trials!");
-          break;
-        case 3:
-          System.out.println("MSQ~~~!!!");
-          break;
-        case 4:
-          System.out.println("Alliance raids");
-          break;
-        case 5:
-          System.out.println("DESCENDING MISERY!");
-          break;
-        case 6:
-          System.out.println("DESTINY CHASING TIME!");
-          break;
-        default:
-          System.out.println("I AM THE STORM THAT IS APPROACHING!");
-      }
-      System.out.println("Based on your answer we think you should run: " + "[Placeholder for duty to be run]" + "!");
+            String welcome = "This is not an ukemi!";
+            System.out.println(welcome);
+            Random rand = new Random();
+            int rouletteCap = 6;
+            //
+            int rouletteResult = rand.nextInt(rouletteCap);
+            System.out.println(rouletteResult);
 
-      System.out.println("Would you like to run this one or try again? :) ");
+            switch(rouletteResult){
+              case 1:
+                System.out.println("Leveling roulette!");
+                break;
+              case 2:
+                System.out.println("Trials!");
+                break;
+              case 3:
+                System.out.println("MSQ~~~!!!");
+                break;
+              case 4:
+                System.out.println("Alliance raids");
+                break;
+              case 5:
+                System.out.println("DESCENDING MISERY!");
+                break;
+              case 6:
+                System.out.println("DESTINY CHASING TIME!");
+                break;
+              default:
+                System.out.println("I AM THE STORM THAT IS APPROACHING!");
+            }
+
+            System.out.println("Based on your answer we think you should run: " + "[Placeholder for duty to be run]" + "!");
       //accept user input for try again or end the randomizer loop
       //find some way to keep track of daily roulettes run so they don't have to input them each time
       //from there we find some kind of way to drop the stored data for the day at daily reset based on server time
