36041da5ff5a813d4127c8e07a90023997b86083
Zach <zach.t.gonzales@gmail.com>
2026-03-04 19:34:00 -0600

    Initial Commit for addition of Janken

---
+++ Janken/Janken.py
@@ -0,0 +1,19 @@
+"""Janken Game
+
+CS101 project for codecademy:
+
+Janken!
+
+Here's what we're gonna do:
+
+First we're gonna prompt the user to input either (R)ock, (P)aper or (S)cissors
+The program/CPU opponent will play their hand as well
+If the user wins, opponent will ask for a rematch(Potentially we can up this to 2 of 3, then 3 out of 5, etc.)
+If the user loses, opponent will ask if they want to go again(potentially we can up this to 2 out of 3, etc.)
+If the user ties, "Aikou deshou!" and CPU will go back to ask for (R)ock, (P)aper or (S)cissors
+
+We'll do this utilizing my jp knowledge and RPS knowledge, I don't think we'll need a class per se this is just gonna be a text script that runs until the user gets bored/tired or wins. 
+
+Quick and easy, let's get it done.
+
+"""
